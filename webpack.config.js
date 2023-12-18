0fbf5089e2caaf0856fdcf7a10a69fd38015bb4d
Ramzan Viskhanov <ramzanviskhanov@mail.ru>
2023-12-18 11:06:57 +0300

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: production ? '[name].[contenthash].js' : '[name].js',
-    publicPath: "/",
+    publicPath: "./",
   },
   target: "web",
   resolve: {
